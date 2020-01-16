5137dc0c70dcf66a5feba51b7e99475e147412fa
meghanduprey <meghan.duprey@gmail.com>
2020-01-16 14:31:50 -0500

    trying to get stylesheet to connect

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php require_once('private/initialize.php'); ?>
+<?php $page_title = 'Tool Library'; ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
--- private/shared/header.php
+++ private/shared/header.php
@@ -4,18 +4,25 @@
   <head>
     <title>Tool Library - <?php echo h($page_title); ?></title>
     <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/styles.css'); ?>" />
+    <link rel="stylesheet" media="all" href="../../public/stylesheets/styles.css" >
 <!--    <script src='https://www.google.com/recaptcha/api.js' async defer></script>-->
   </head>
 
   <body>
   <div id="wrapper">
-
-    <nav>
+    <nav class="header">
+     <a href="../../index.php" class="logo">Jackson Park Tool Library</a>
+      <div class="header-right">
+        <a href="../../about.php">About</a>
+        Browse
+        <a href="../../public/login.php">Login</a>
+      </div>
+    </nav>
+    <div>
       <ul>
         <li>User: <?php echo $_SESSION['email'] ?? '';   ?></li>
         <li><a href="<?php echo url_for('/index.php'); ?>">Menu</a></li>
         <li><a href="<?php echo url_for('/logout.php'); ?>">Logout</a></li>
       </ul>
-    </nav>
+    </div>
     <?php echo display_session_message(); ?>
